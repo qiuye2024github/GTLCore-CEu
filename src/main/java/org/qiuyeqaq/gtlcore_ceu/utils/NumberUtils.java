54e58a8c10f12fe00bfd61f59bdd4ce09c2f010c
qiuye2024github <qiuye2024github@163.com>
2025-04-15 17:27:29 +0800

    ae2更改

---
+++ src/main/java/org/qiuyeqaq/gtlcore_ceu/utils/NumberUtils.java
@@ -0,0 +1,41 @@
+package org.qiuyeqaq.gtlcore_ceu.utils;
+
+import com.google.common.math.LongMath;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+
+import java.text.DecimalFormat;
+
+public class NumberUtils {
+
+    private static final String[] UNITS = { "", "K", "M", "G", "T", "P", "E", "B" };
+
+    public static String formatLong(long number) {
+        DecimalFormat df = new DecimalFormat("#.##");
+        double temp = number;
+        int unitIndex = 0;
+        while (temp >= 1000 && unitIndex < UNITS.length - 1) {
+            temp /= 1000;
+            unitIndex++;
+        }
+        return df.format(temp) + UNITS[unitIndex];
+    }
+
+    public static MutableComponent numberText(long number) {
+        return Component.literal(formatLong(number));
+    }
+
+    public static int getFakeVoltageTier(long voltage) {
+        long a = voltage;
+        int b = 0;
+        while (a / 4L >= 8L) {
+            b++;
+            a /= 4L;
+        }
+        return b;
+    }
+
+    public static long getVoltageFromFakeTier(int tier) {
+        return LongMath.pow(4L, tier + 1) * 2;
+    }
+}
